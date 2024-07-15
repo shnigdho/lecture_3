672a23be2f930cd2e7b820bb5d11bc7b6c5bdad5
Your Name <you@example.com>
2024-07-15 21:20:31 +0600

    PATTERN_2

---
+++ pattren_2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // Your code here
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        int j = 1;
+        while(j<=i){
+            cout<<i;
+            j = j + 1;
+            }
+            cout<<endl;
+            i = i + 1;
+
+    }
+
+ 
+}
